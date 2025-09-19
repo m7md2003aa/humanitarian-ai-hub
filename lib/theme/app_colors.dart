c6a867f27e4d1515e5073cba2e52c5eb2fc783ed
moham <mohammed2003c@gmail.com>
2025-09-20 00:41:04 +0400

    Initial Flutter app

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -5,10 +5,10 @@ class AppColors {
   static const primary = Color(0xFF15956F);
   static const secondary = Color(0xFFFFB020);
   static const tertiary = Color(0xFF2E7DCA);
-//Hala MADRID
+
   static const background = Color(0xFFF7F9FA);
   static const surface = Colors.white;
   static const text = Color(0xFF1E293B);
   static const textMuted = Color(0xFF6B7280);
-  //Ameena The Best
+  //Ameena The worst
 }
